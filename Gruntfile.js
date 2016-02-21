0e3a065f42b6a5ccd5e440dcd87f919e75d04261
XhmikosR <xhmikosr@gmail.com>
2016-02-21 12:15:37 +0200

    Remove JSHint from default task.
    It's in test task, and test task is in default, so it's redundant.

--- Gruntfile.js
+++ Gruntfile.js
@@ -174,6 +174,6 @@ module.exports = function(grunt) {
   grunt.registerTask('test', ['jshint', 'clean', 'concat', 'nodeunit']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test', 'build-contrib']);
+  grunt.registerTask('default', ['test', 'build-contrib']);
 
 };
